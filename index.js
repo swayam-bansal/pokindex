02471ee615db4babb6c0dae1bb19c7c74e4d0b92
Swayam Bansal <swayambansal@outlook.com>
2024-05-22 21:53:42 +0530

    Nothing much

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const baseUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-vii/ultra-sun-ultra-moon/"
 
+const container = document.querySelector('#container')
 
+const gen1label = document.createElement('h2')
+gen1label.innerText = "Generation-1"
+container.appendChild(gen1label)
 
-for (let i = 1; i <= 100; i++) {
+for (let i = 1; i <= 151; i++) {
     const div = document.createElement("div");
     div.setAttribute('class', 'pokeCard')
     const img = document.createElement("img");
@@ -12,5 +16,22 @@ for (let i = 1; i <= 100; i++) {
     label.innerText = `#${i}`
     div.appendChild(img);
     div.appendChild(label);
-    document.querySelector('#container').appendChild(div)
+    container.appendChild(div)
+}
+
+const gen2label = document.createElement('h2')
+gen2label.innerText = "Generation-2"
+container.appendChild(gen2label)
+
+for (let i = 152; i <= 251; i++) {
+    const div = document.createElement("div");
+    div.setAttribute('class', 'pokeCard')
+    const img = document.createElement("img");
+    img.setAttribute('class', 'pokeImg')
+    img.src = `${baseUrl}${i}.png`;
+    const label = document.createElement('span')
+    label.innerText = `#${i}`
+    div.appendChild(img);
+    div.appendChild(label);
+    container.appendChild(div)
 }
